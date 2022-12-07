bb798596f65d5f806da5dcf8a1dad31e9133b363
Spr1ng0n10n <jacobeonion@gmail.com>
2022-12-07 10:33:00 +0000

    k

--- Planet of the Shapes/Assets/Scripts/RotateGun.cs
+++ Planet of the Shapes/Assets/Scripts/RotateGun.cs
@@ -6,11 +6,9 @@ public class RotateGun : MonoBehaviour
 {
     Vector3 mousePos;
     private Camera mainCamera;
-    //private Rigidbody2D rb;
-    //public GameObject gun;
     public float offset;
     private Transform player;
-    // Start is called before the first frame update
+
     void Awake()
     {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
@@ -30,8 +28,8 @@ public class RotateGun : MonoBehaviour
         Vector2 pos = mousePos - player.position;
         float angle = Mathf.Atan2(pos.y, pos.x); //Finds the angle of the mouse compared to the player
 
-        transform.localPosition = new Vector3(Mathf.Cos(angle) * offset, Mathf.Sin(angle) * offset, 0); //explain in doc
-        transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg - 90f);
+        transform.localPosition = new Vector3(Mathf.Cos(angle) * offset, Mathf.Sin(angle) * offset, 0); //moves the gun
+        transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg - 90f); // rotates the gun on the Z axis
 
     }
 }
